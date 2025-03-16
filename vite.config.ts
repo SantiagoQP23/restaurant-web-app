869b89468c3b263e53b378748d491803fdaf3021
Steven Rosales <stevenrosales31@gmail.com>
2025-03-16 15:18:22 -0500

    Enhance Vite configuration:  optimization to drop console and debugger statements.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+/**
+ * Vite configuration
+ * @author Steven Rosales
+ * @version 1.0 17/03/2025 Hide console and debugger in production
+ */
 export default defineConfig({
   plugins: [react({
     include: "**/*.tsx",
@@ -16,5 +21,10 @@ export default defineConfig({
   },
   preview: {
     port: 8080
-  }
+  },
+  ...(process.env.NODE_ENV === "production" && {
+     esbuild: {
+      drop: ['console', 'debugger'],
+    },
+  }),
 })
